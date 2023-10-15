560abcb14541afec39fc910c30db134bc202b39f
Tobexxy <tobiogunleye76@gmail.com>
2023-10-15 16:36:09 +0100

    updated codes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.querySelector("input").click()
 
 var list = document.firstElementChild.lastElementChild.firstElementChild.nextElementSibling;
 list.innerHTML = "Java Testing";
-list.style.color = "red"
+
 
 
 //how to get an element with multiple items and same name e.g h1 or l1 appearing multiple times .getting elements by their tag name
@@ -30,3 +30,10 @@ document.querySelectorAll("li")[0]; //use query selector to get a specific tag w
 //changing an item in the list color
 document.querySelector("ul li a").style.color = "red";
 
+//changing color of button
+document.querySelector("button").style.backgroundColor = "red";
+
+document.querySelector("button").style.color = "white";
+
+//creating a class list in Javascript and adding it to through css
+document.querySelector("h2").classList.add("test");
